8a9f5910418cb781070991515041fe31aac5c0c5
yqianjiang <y.jiang.psy@gmail.com>
2023-10-23 01:03:36 +0800

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -21,9 +21,7 @@ export function updateLeaderboard(level) {
     leaderboard[level].innerHTML = "";
     
     // 排序并显示前N名成绩（假设显示前5名）
-    const topScores = scores
-        .sort((a, b) => a.time - b.time)
-        .slice(0, 5);
+    const topScores = scores.slice(0, 5);
 
     topScores.forEach((score, index) => {
         const listItem = document.createElement("li");
@@ -37,18 +35,19 @@ export function submitScore(level, playerTime) {
     level = level.toLowerCase();
     // 从localStorage中获取英雄榜数据（如果有的话）
     const scores = JSON.parse(localStorage.getItem(`${level}-scores`)) || [];
-    const topScores = scores.sort((a, b) => a.time - b.time).slice(0, 5);
-    if (topScores.length<5 || topScores.some(x=>playerTime<x.time)) {
+    const topScores = scores.slice(0, 5);
+    if (topScores.length<5 || playerTime<topScores.slice(-1)[0].time) {
         const playerName = prompt("新记录！请在英雄榜留下你的名字（可选）");
         if (!playerName) {
             return;  // 用户不想输入名字
         }
         // 将新成绩添加到初级英雄榜
         const day = new Date();
-        topScores.push({ name: playerName, time: playerTime, date: day.toLocaleDateString()});
+        const curr = { name: playerName, time: playerTime, date: day.toLocaleDateString()};
+        const idx = searchInsert(topScores, playerTime);
     
         // 保存数据到localStorage
-        localStorage.setItem(`${level}-scores`, JSON.stringify(topScores));
+        localStorage.setItem(`${level}-scores`, JSON.stringify([...topScores.slice(0, idx), curr, ...topScores.slice(idx)]));
     
         // 更新初级英雄榜
         updateLeaderboard(level);
@@ -59,3 +58,21 @@ export function submitScore(level, playerTime) {
 updateLeaderboard("beginner");
 updateLeaderboard("intermediate");
 updateLeaderboard("expert");
+
+
+function searchInsert(nums, target) {
+    // nums有序：二分法
+    let left = 0, right = nums.length -1;
+    while (left <= right) {
+        const middle = left + Math.floor((right - left) / 2);
+        if (target === nums[middle].time) {
+            return middle;
+        }
+        if (target > nums[middle].time) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return left;
+};
