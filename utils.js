a878d09a5817c1c9bc9ff4c2ae88c2779fab39f1
yqianjiang <48210973+yqianjiang@users.noreply.github.com>
2023-11-21 17:10:48 +0000

    Deploying to gh-pages from @ yqianjiang/minesweeper@0c5d2544eae895f6fb60a8d104202beaa7d56181 🚀

--- utils.js
+++
@@ -1,6 +0,0 @@
-export const checkClickBtn = ([cx, cy], btn) => {
-    const scaleFactor = 1;
-    cx /= scaleFactor;
-    cy /= scaleFactor;
-    return cx > btn.x && cx < btn.x + btn.w && cy > btn.y && cy < btn.y + btn.h;
-}
