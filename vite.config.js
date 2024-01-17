221b3407e0671ec0bf5b8411fdfbf590445994fb
yqianjiang <y.jiang.psy@gmail.com>
2024-01-17 22:03:19 +0800

    chrome: use gzip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "https://webgames.fun/minesweeper/",
+  plugins: [
+    viteCompression({
+      //生成压缩包.gz/.br
+      verbose: true,
+      disable: false,
+      threshold: 1024,
+      algorithm: 'gzip',
+      ext: '.gz',
+      // algorithm: 'brotliCompress',
+      // ext: '.br',
+    }),
+  ]
 });
