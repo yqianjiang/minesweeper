880435185f490ba16ac84f4ddfb243ee45b07809
yqianjiang <y.jiang.psy@gmail.com>
2024-01-17 21:58:09 +0800

    fix: uuid compact old browser

--- userInfo.js
+++ userInfo.js
@@ -1,12 +1,29 @@
 const infoKey = 'minesweeper';
 
+function generateUUID() {
+  // 能力检测
+  if ("crypto" in window && "randomUUID" in window.crypto) {
+    return window.crypto.randomUUID();
+  }
+
+  // 兼容性处理
+  // https://stackoverflow.com/a/2117523/1293256
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line no-mixed-operators
+    const r = (Math.random() * 16) | 0;
+    // eslint-disable-next-line no-mixed-operators,eqeqeq
+    const v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 class UserInfo {
   constructor() {
     this.name = '';
     this.loadFromStorage();
     // 若该用户还没有 uuid，生成一个唯一的 uuid
     if (!this.uuid) {
-      this.uuid = window.crypto.randomUUID();
+      this.uuid = generateUUID();
       this.saveToStorage();
     }
   }
